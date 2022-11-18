fdf8f4ec76614165406c5e1ccc51c8ede5230fd8
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-11-18 12:30:35 +0000

    add changeble  request

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const {errorHanler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
+const colors = require('colors')
+const connectDB = require('./config/db')
 
+connectDB()
 const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
